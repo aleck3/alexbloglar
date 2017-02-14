622a1f4aaeac85db5685cded01e2d5b84745eea6
aleck3 <aleck3@ukr.net>
2017-02-14 10:10:40 +0200

    Alexbloglar is complete

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,8 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Styles -->
-        <link href="/css/app.css" rel="stylesheet">
-
+        <link href="/css/app.css" rel="stylesheet" type="text/css">
+        <link href="/css/mystyle.css" rel="stylesheet" type="text/css">
         <!-- Scripts -->
         <script>
             window.Laravel = <?php
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,26 +1,34 @@
 @extends('layouts.app')
 @section('content')
-<form action="post/search" method="post">
-    {{ csrf_field() }}
-    <div class="input-group">
-        <input type="text" class="form-control" name="q"
-               placeholder="Search posts"> <span class="input-group-btn">
-            <button type="submit" class="btn btn-default">
-                <span class="glyphicon glyphicon-search"></span>
-            </button>
-        </span>
+</div>
+<div class="jumbotron jumbo">
+    <div class="container">
+        <h1>Blog Theme</h1>
+        <p>Pacific herring codlet crocodile shark</p>
     </div>
-</form>
-<h1> Posts </h1>
-<a href="{{ action('PostController@addpost') }}">Create new Post</a><br>
-<a href="{{ action('PostController@contact') }}">Contact Us</a><hr>
-<ul class="list-unstyled">
-    @foreach ($posts as $post)
-    <li><a href="{{ action('PostController@showpost', ['id' => $post->id]) }}"><h2>{{ $post->title }}</h2></a></li>
-    <li><i>{{ $post->user->name }}</i></li>
-    <li>{{ str_limit($post->content, 200) }}</li>
-    <li><b>{{ date('F d, Y', strtotime($post->date_published)) }}</b></li><br>
-    @endforeach
-</ul>
-{{ $posts->links() }}
-@endsection
+</div>
+<div class="container">
+    <form action="post/search" method="post">
+        {{ csrf_field() }}
+        <div class="input-group">
+            <input type="text" class="form-control" name="q"
+                   placeholder="Search posts"> <span class="input-group-btn">
+                <button type="submit" class="btn btn-default">
+                    <span class="glyphicon glyphicon-search"></span>
+                </button>
+            </span>
+        </div>
+    </form>
+    <h1> Posts </h1>
+    <a href="{{ action('PostController@addpost') }}">Create new Post</a><br>
+    <a href="{{ action('PostController@contact') }}">Contact Us</a><hr>
+    <ul class="list-unstyled">
+        @foreach ($posts as $post)
+        <li><a href="{{ action('PostController@showpost', ['id' => $post->id]) }}"><h2>{{ $post->title }}</h2></a></li>
+        <li><i>{{ $post->user->name }}</i></li>
+        <li>{{ str_limit($post->content, 200) }}</li>
+        <li><b>{{ date('F d, Y', strtotime($post->date_published)) }}</b></li><br>
+        @endforeach
+    </ul>
+    {{ $posts->links() }}
+    @endsection
